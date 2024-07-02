decdcec339750bdbf38c01594e2fee1cad23206a
Runkang Chen <chen.runkang10@gmail.com>
2024-07-02 18:26:54 +0200

    v3.0.0
    SyS App website v3.0.0 dev update. This update is not stable.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 
+const getAuthorizedOrigins = () => {
+    const origins = process.env.NEXT_PUBLIC_BASE_URL;
+    if (!origins) {
+        throw new Error('Environment variables do not contain authorized URLs.');
+    }
+    return origins;
+};
+
+const authorizedOrigins = getAuthorizedOrigins();
+
 const nextConfig = {
-    
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                destination: authorizedOrigins,
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
