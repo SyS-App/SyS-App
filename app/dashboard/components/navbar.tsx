0b5918421113b639508b7c9c232a26d6fbdd6122
Runkang Chen <chen.runkang10@gmail.com>
2024-07-07 12:23:44 +0200

    Update navbar.tsx

--- app/dashboard/components/navbar.tsx
+++ app/dashboard/components/navbar.tsx
@@ -1,8 +1,13 @@
+import { Logo } from "@/components/logo/default";
 
 const DashboardNavBar = () => {
     return (
-        <header className="">
-
+        <header className="fixed top-[60px] left-0 p-4 w-4">
+            {/* <nav className="w-full h-full bg-black">
+                <div className="flex flex-col">
+                    <Logo width={32} height={32} />
+                </div>
+            </nav> */}
         </header>
     )
 }
