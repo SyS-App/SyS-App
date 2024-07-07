105d64d3f128d8d2d9430799842c32dbf30c12ab
Runkang Chen <chen.runkang10@gmail.com>
2024-07-07 16:59:29 +0200

    Update alert.tsx

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,11 +1,11 @@
+// Alert component (Not finished yet)
 "use client"
-
-// Import (External)
-import { X } from "lucide-react";
-import { useState } from "react";
 // Import (Internal)
 import AlertData from "@/alert.json";
 import { AlertProps } from "@/types/alert";
+// Import (External)
+import { X } from "lucide-react";
+import { useState } from "react";
 
 function AlertCore() {
     const [alerts, setAlerts] = useState<AlertProps[]>(AlertData);
@@ -24,7 +24,7 @@ function AlertCore() {
             {alerts.map((alert: AlertProps) => (
                 alert.visible && (
                     <div className="h-[60px] flex justify-center border items-center px-4 text-sm danger bg-background" key={alert.id}>
-                        <div />
+                        {alert.content}
                     </div>
                 )
             ))}
